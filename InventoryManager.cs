734211d8c4002fd5095f0f94a876c481206da6e4
Akaina1 <erinadost@gmail.com>
2023-08-16 14:09:56 -0400

    reworked inventory / product class

--- InventoryManager.cs
+++ InventoryManager.cs
@@ -5,29 +5,23 @@ namespace FinalProject
 {
     public class InventoryManager
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public double Price { get; set; } // in Dollars
-        public double Cost { get; set; } // in Dollars
-        public int InStock { get; set; }
-        public int SoldLastMonth { get; set; }
-
         public void ShowInventory() // read from csv file
         {
             using (var reader = new StreamReader("D:\\School\\School Work Code\\Udemy Code\\(3) C# Advanced Topics\\C# Final Project\\FinalProject\\Database\\Inventory.csv")) // open file
             using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) // read file
             {
-                var records = csv.GetRecords<InventoryManager>().ToList(); // convert to list
+                var records = csv.GetRecords<Product>().ToList(); // convert to list
 
                 // print out each record in formatted table to console
 
-                Console.WriteLine("Id\tName\t\t\tPrice\tCost\t\tInStock\tSoldLastMonth");
-                Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.WriteLine($"{"Id",-7} {"Name",-23} {"Price",-15} {"Cost",-15} {"InStock",-15} {"SoldLastMonth",-10}");
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
                 foreach (var record in records)
                 {
-                    Console.WriteLine($"{record.Id}\t{record.Name}\t\t${record.Price}\t${record.Cost}\t\t{record.InStock}\t{record.SoldLastMonth}");
+                    // use getters to access private fields in Product class
+                    Console.WriteLine($"{record.Id,-5}\t{record.Name,-20}\t${record.Price,-10:F2}\t${record.Cost,-7:F2}\t{record.InStock,-8}\t{record.SoldLastMonth,-13}");
                 }
-                Console.WriteLine("-----------------------------------------------------------------------------");
+                Console.WriteLine("---------------------------------------------------------------------------------------------");
             }
         }
     }
---
+++ Product.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics;
+
+namespace FinalProject
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; } // in Dollars
+        public double Cost { get; set; } // in Dollars
+        public int InStock { get; set; }
+        public int SoldLastMonth { get; set; }
+    }
+}
