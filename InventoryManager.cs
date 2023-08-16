ffd09b3afd7e88fc2404521b2798b892f4602cd1
Akaina1 <erinadost@gmail.com>
2023-08-16 12:28:45 -0400

    Add project files.

---
+++ InventoryManager.cs
@@ -0,0 +1,34 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace FinalProject
+{
+    public class InventoryManager
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; } // in Dollars
+        public double Cost { get; set; } // in Dollars
+        public int InStock { get; set; }
+        public int SoldLastMonth { get; set; }
+
+        public void ShowInventory() // read from csv file
+        {
+            using (var reader = new StreamReader("D:\\School\\School Work Code\\Udemy Code\\(3) C# Advanced Topics\\C# Final Project\\FinalProject\\Database\\Inventory.csv")) // open file
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) // read file
+            {
+                var records = csv.GetRecords<InventoryManager>().ToList(); // convert to list
+
+                // print out each record in formatted table to console
+
+                Console.WriteLine("Id\tName\t\t\tPrice\tCost\t\tInStock\tSoldLastMonth");
+                Console.WriteLine("-----------------------------------------------------------------------------");
+                foreach (var record in records)
+                {
+                    Console.WriteLine($"{record.Id}\t{record.Name}\t\t${record.Price}\t${record.Cost}\t\t{record.InStock}\t{record.SoldLastMonth}");
+                }
+                Console.WriteLine("-----------------------------------------------------------------------------");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace FinalProject
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // test inventory manager method
+            InventoryManager inventoryManager = new(); // can just use new() instead of new InventoryManager() to instantiate a new object - C# 9.0 feature
+            inventoryManager.ShowInventory();
+        }
+    }
+}
