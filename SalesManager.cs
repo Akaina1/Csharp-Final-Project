89824652575e335949c2cd063bec36095f34fe48
Akaina1 <erinadost@gmail.com>
2023-08-16 16:20:29 -0400

    Created SalesOrder and implemnted ShowSales method

--- SalesManager.cs
+++ SalesManager.cs
@@ -1,7 +1,28 @@
-﻿namespace FinalProject
+﻿using CsvHelper;
+using System.Globalization;
+
+namespace FinalProject
 {
     public class SalesManager
     {
+        public void ShowSales()
+        {
+            using (var reader = new StreamReader("D:\\School\\School Work Code\\Udemy Code\\(3) C# Advanced Topics\\C# Final Project\\FinalProject\\Database\\Sales.csv")) // open file
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture)) // read file
+            {
+                var records = csv.GetRecords<SalesOrder>().ToList(); // convert to list
 
+                // print out each record in formatted table to console
+
+                Console.WriteLine($"{"OrderId",-7} {"ProductName",-23} {"SalePrice",-15} {"Status",-15}");
+                Console.WriteLine("--------------------------------------");
+                foreach (var record in records)
+                {
+                    // use getters to access private fields in Product class
+                    Console.WriteLine($"{record.OrderId,-5}\t{record.ProductName,-20}\t${record.SalePrice,-10:F2}\t{record.OrderStatus,-20}");
+                }
+                Console.WriteLine("--------------------------------------");
+            }
+        }
     }
 }
---
+++ SalesOrder.cs
@@ -0,0 +1,11 @@
+﻿namespace FinalProject
+{
+    public class SalesOrder
+    {
+        public int OrderId { get; set; }
+        public string ProductName { get; set; }
+        public double SalePrice { get; set; } // in Dollars
+        public enum Status { Pending, Delayed, Shipped, Delivered }
+        public Status OrderStatus { get; set; }
+    }
+}
